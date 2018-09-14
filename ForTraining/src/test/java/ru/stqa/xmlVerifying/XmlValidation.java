a6a3b2a54b85b9d9db3bb77d87f729f9087cafac
DEX-6 <irina14.91@bk.ru>
2018-09-14 18:04:18 +0400

    xmlUnitTests

--- ForTraining/src/test/java/ru/stqa/xmlVerifying/XmlValidation.java
+++ ForTraining/src/test/java/ru/stqa/xmlVerifying/XmlValidation.java
@@ -1,118 +1,119 @@
 package ru.stqa.xmlVerifying;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Test;
+import org.w3c.dom.Node;
 import org.xmlunit.builder.DiffBuilder;
-import org.xmlunit.diff.DefaultNodeMatcher;
-import org.xmlunit.diff.Diff;
-import org.xmlunit.diff.Difference;
-import org.hamcrest.number.OrderingComparison;
-import org.xmlunit.diff.ElementSelectors;
-import org.xmlunit.matchers.CompareMatcher;
-import ru.stqa.xmlVerifying.differenceEvaluators.IgnoreAttributeDifferenceEvaluator;
+import org.xmlunit.diff.*;
+import org.xmlunit.util.Predicate;
 
-import java.util.Iterator;
+import java.io.InputStream;
 
-import static org.hamcrest.CoreMatchers.not;
-//import static org.junit.Assert.assertThat;
-import static org.xmlunit.matchers.CompareMatcher.isIdenticalTo;
+import static org.xmlunit.diff.ElementSelectors.byName;
+import static org.xmlunit.diff.ElementSelectors.byNameAndText;
 
 
 public class XmlValidation {
     @Test
-    public void given2XMLS_whenIdentical_thenCorrect() {
-        String controlXml = "<struct><int>3</int><boolean>false</boolean></struct>";
-        String testXml = "<struct><int>3</int><boolean>false</boolean></struct>";
-        Assert.assertThat(testXml, isIdenticalTo(controlXml));
+    public void given2XMLsWithDifferencesInNestedNodes_2() {
+        InputStream control = this.getClass().getClassLoader().getResourceAsStream("control.xml");
+        InputStream test = this.getClass().getClassLoader().getResourceAsStream("test.xml");
+
+        Diff myDiff = DiffBuilder.compare(control)
+                .withTest(test)
+                .ignoreComments()
+                .ignoreWhitespace()
+                .ignoreElementContentWhitespace()
+                .checkForIdentical()
+                .checkForSimilar()
+                .withComparisonController(ComparisonControllers.Default)
+                .withNodeMatcher(
+                        new DefaultNodeMatcher
+                                (ElementSelectors.conditionalBuilder()
+                                        .whenElementIsNamed("tr")
+                                        .thenUse(ElementSelectors.byXPath("./th", byNameAndText))
+                                        .elseUse(byName).build()
+                                        , ElementSelectors.conditionalBuilder().whenElementIsNamed("tr")
+                                        .thenUse(ElementSelectors.byXPath("./td", byNameAndText))
+                                        .elseUse(byName).build()
+                                )
+                )
+                .build();
+
+        printDifferences(myDiff);
+
     }
 
     @Test
-    public void given2XMLSWithSimilarNodesButDifferentSequence_whenNotIdentical_thenCorrect() {
-        String controlXml = "<struct><int>3</int><boolean>false</boolean></struct>";
-        String testXml = "<struct><boolean>false</boolean><int>3</int></struct>";
-        Assert.assertThat(testXml, not(isIdenticalTo(controlXml)));
+    public void differencesBetweenByNameAndByNameAndText() {
+        InputStream control = this.getClass().getClassLoader().getResourceAsStream("control.xml");
+        InputStream test = this.getClass().getClassLoader().getResourceAsStream("test.xml");
+
+        Diff myDiff = DiffBuilder.compare(control)
+                .withTest(test)
+                .ignoreComments()
+                .ignoreWhitespace()
+                .ignoreElementContentWhitespace()
+                .checkForIdentical()
+                .checkForSimilar()
+                .withComparisonController(ComparisonControllers.Default)
+//                .withNodeMatcher(
+//                        new DefaultNodeMatcher
+//                                (ElementSelectors.conditionalBuilder()
+//                                     //  "Когда находим элемент tr С наследником th
+//                                     // сравниваем tr по названию элементов и тексту"
+//                                        .whenElementIsNamed("tr")
+//                                        .thenUse(ElementSelectors.byXPath("./th", byNameAndText))
+//                                        .elseUse(byName).build()
+//                                )
+//                )
+                .withNodeFilter(
+                        node -> (node.getNodeName().equals("b"))
+                )
+                .build();
+
+        printDifferences(myDiff);
+
     }
 
-    @Test
-    public void given2XMLS_whenGeneratesDifferences_thenCorrect(){
-        String controlXml = "<struct><int>3</int><boolean>false</boolean></struct>";
-        String testXml = "<struct><boolean>false</boolean><int>3</int></struct>";
-        Diff myDiff = DiffBuilder.compare(controlXml).withTest(testXml).build();
-
-        Iterator<Difference> iter = myDiff.getDifferences().iterator();
-        int size = 0;
-        while (iter.hasNext()) {
-            iter.next().toString();
-            size++;
+    private void printDifferences(Diff myDiff) {
+        int i = 1;
+        System.out.println("---------------------------------------------------------------------");
+        for (Difference diff : myDiff.getDifferences()) {
+            System.out.println(i++);
+            System.out.println("RESULT: " + diff.getComparison());
+            System.out.println("TEST XPATH: " + diff.getComparison().getTestDetails().getXPath());
+            System.out.println("CONTROL XPATH: " + diff.getComparison().getControlDetails().getXPath());
+            System.out.println("TEST VALUE: " + diff.getComparison().getTestDetails().getValue());
+            System.out.println("CONTROL VALUE: " + diff.getComparison().getControlDetails().getValue());
+            System.out.println("---------------------------------------------------------------------");
         }
-        Assert.assertThat(size, OrderingComparison.greaterThan(1));
-    }
-
-//    5. Comparing Specific Nodes
-    @Test
-    public void given2XMLS_whenSimilar_thenCorrect() {
-        String controlXml = "<struct><int>3</int><boolean>false</boolean></struct>";
-        String testXml = "<struct><boolean>false</boolean><int>3</int></struct>";
-
-        Assert.assertThat(testXml,
-                CompareMatcher.isSimilarTo(controlXml)
-                        .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byName)));
+        System.out.println("Наличие различий " + myDiff.hasDifferences());
     }
 
     @Test
-    public void given2XMLS_whenSimilar_thenCorrect_2() {
-        String controlXml = "<struct><int>3</int><boolean>false</boolean></struct>";
-        String testXml = "<struct><int>4</int><boolean>false</boolean></struct>";
+    public void getDifferences() {
+        String controlXml = "<struct>" +
+                "<boolean>false</boolean>" +
+                "<int>4</int>" +
+                "<int>3</int>" +
+                "</struct>";
+        String testXml = "<struct>" +
+                "<int>3</int>" +
+                "<int>4</int>" +
+                "<boolean>false</boolean>" +
+                "</struct>";
 
-        Assert.assertThat(testXml,
-                CompareMatcher.isSimilarTo(controlXml)
-                        .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndText)));
+        Diff myDiff = DiffBuilder.compare(controlXml).withTest(testXml).checkForSimilar().withNodeMatcher(new DefaultNodeMatcher(byNameAndText)).build();
+        for (Difference diff : myDiff.getDifferences()) {
+            System.out.println("test comparison: " + diff.getComparison());
+            System.out.println("test target: " + diff.getComparison().getTestDetails().getTarget());
+            System.out.println("test value: " + diff.getComparison().getTestDetails().getValue());
+            System.out.println("test comparison type: " + diff.getComparison().getType());
+            System.out.println("test result:" + diff.getResult());
+        }
+
+        System.out.println("has differences " + myDiff.hasDifferences());
+
     }
-
-
-//    6. Custom DifferenceEvaluator
-@Test
-public void given2XMLsWithDifferences_whenTestsDifferentWithoutDifferenceEvaluator_thenCorrect() {
-    final String control = "<a><b attr=\"abc\"></b></a>";
-    final String test = "<a><b attr=\"xyz\"></b></a>";
-    Diff myDiff = DiffBuilder.compare(control).withTest(test)
-            .withDifferenceEvaluator(new IgnoreAttributeDifferenceEvaluator("attr"))
-            .checkForSimilar().build();
-    Assert.assertFalse(myDiff.toString(), myDiff.hasDifferences());
-}
-
-@Test
-public void given2XMLsWithDifferencesInNestedNodes(){
-        String control = "<table>\n" +
-                "  <tbody>\n" +
-                "    <tr>\n" +
-                "      <th>some key</th>\n" +
-                "      <td>some value</td>\n" +
-                "    </tr>\n" +
-                "    <tr>\n" +
-                "      <th>another key</th>\n" +
-                "      <td>another value</td>\n" +
-                "    </tr>\n" +
-                "  </tbody>\n" +
-                "</table>";
-        String test = "<table>\n" +
-                "  <tbody>\n" +
-                "    <tr>\n" +
-                "      <th>another key</th>\n" +
-                "      <td>another value</td>\n" +
-                "    </tr>\n" +
-                "    <tr>\n" +
-                "      <th>some key</th>\n" +
-                "      <td>some value</td>\n" +
-                "    </tr>\n" +
-                "  </tbody>\n" +
-                "</table>";
-    Diff myDiff = DiffBuilder.compare(control).withTest(test)
-            .withNodeMatcher(new DefaultNodeMatcher
-                    (ElementSelectors.byXPath("tr/th", ElementSelectors.byNameAndText)))
-            .checkForSimilar().build();
-    Assert.assertFalse(myDiff.toString(), myDiff.hasDifferences());
-}
 
 }
