21a9311d7312467a60be5b7c5164545f9e2e6c87
DEX-6 <irina14.91@bk.ru>
2019-02-28 20:25:24 +0400

    Заготовка в виде комментария под код

--- ForTraining/src/test/java/mqConnectionTesting/MqConnectionTest.java
+++ ForTraining/src/test/java/mqConnectionTesting/MqConnectionTest.java
@@ -4,6 +4,12 @@ import org.junit.Test;
 
 public class MqConnectionTest {
 
+//    Коннектор
+//    Сессия
+//    Очередь
+//    Сендер (отправитель)
+//    Ресивер (получатель)
+
     @Test
     public void test(){
 
